6f76486561a8688592f6aa98275ea7cf98c942f1
Nathan Ngenoh <nate.kiprotich@gmail.com>
2022-06-14 19:54:00 +0300

    First Exercise and Directory Structure

---
+++ src/InputProcessingOutput/count_characters.py
@@ -0,0 +1,8 @@
+"""
+    Get user input and count number of characters in the string.
+
+    Remove white spaces while counting but maintain string format while returning Output
+"""
+
+
+def 
---
+++ src/InputProcessingOutput/hello.py
@@ -0,0 +1,33 @@
+"""
+    Ask user to Input their name and returns greeting.
+
+    i.e. "Hello John"
+"""
+
+import  datetime
+
+def time_based_greeting():
+    """
+        Greetings based on time of the day
+
+        Output:
+            str - Time based greeting i.e. Good Morning
+    """
+
+    hour = datetime.datetime.now().hour
+
+    if hour >= 0 and hour < 12:
+        return 'Good Morning'
+    elif hour >= 12 and hour < 16:
+        return 'Good Afternoon'
+    elif hour >= 16 and hour < 20:
+        return 'Good Evening'
+    else:
+        return 'Good Night'
+
+
+
+
+greeting = input('Enter your name: ')
+
+print('Hello, {hour_greeting} {greeting}'.format(hour_greeting=time_based_greeting(), greeting=greeting))
